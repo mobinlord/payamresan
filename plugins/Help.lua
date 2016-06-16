ad7aad3e9eae3a9a20d87b857c01eab7dbc277a1
mobinlord <mobinlord29@gmail.com>
2016-06-16 16:43:30 +0430

    Update Help.lua

--- plugins/Help.lua
+++ plugins/Help.lua
@@ -7,13 +7,13 @@ local action = function(msg)
     if string.match(msg.text, '^/help') then
     local text = ''
     if msg.chat.type == 'group' or msg.chat.type == 'supergroup' then
-        text = text..'*WELCOME TO ARMAN PV!*\n[GitHub](github.com/thisisarman)\n_Send Pm With_\n/`/a YourPm!`\n'  --You Can Replace WELCOME TO ARMAN PV!
+        text = text..'*WELCOME TO EMC PV!*\n[EMC](telegram.me/EMCTG)\n_Send Pm With_\n/`/a YourPm!`\n'  --You Can Replace WELCOME TO EMC PV!
 
         sendReply(msg, 'I\'ve send you the requested information in private.\nIf you have never used me before, please start a conversation with me and ask for help here again.')
     end
     
     if msg.chat.type == 'private' then
-        text = text..'*WELCOME TO ARMAN PV!*\n[GitHub](github.com/thisisarman)\n_Send Pm With_\n`/a YourPm!`\n' --You Can Replace WELCOME TO ARMAN PV!
+        text = text..'*WELCOME TO ARMAN EMCPV!*\n[EMC](telegram.me/EMCTG)\n_Send Pm With_\n`/a YourPm!`\n' --You Can Replace WELCOME TO ARMAN PV!
     end
     sendMessage(msg.from.id, text, true, false, true)
 end
